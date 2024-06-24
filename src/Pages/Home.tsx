65473b8d03e8d4057c2c402bfd113fb87ab03794
Dean Kinyua <kinyuadean@gmail.com>
2024-06-24 20:48:16 +0300

    added a inset to my header at large screen sizes

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,11 +1,18 @@
 import harpicVideo from "/Videos/HARPIC Door to Door.mp4"
 
 const Home = () => {
-  return <div className="hero_video_container">     
-<video className="hero_video" src={harpicVideo} autoPlay loop muted></video>
-     </div>
+  return(
+     <div className="hero_video_container">  
 
+     <div className="overlay1"></div>  
+
+     <video className="hero_video" src={harpicVideo} autoPlay loop muted></video>
+       </div>
+  
+  )
 
 };
 
 export default Home;
+
+
