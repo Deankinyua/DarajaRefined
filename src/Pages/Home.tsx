e0799fc838456933ed10a330536f06213b2b818f
Dean Kinyua <kinyuadean@gmail.com>
2024-07-01 11:14:21 +0300

    updated and cut the components folder and the utils

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,5 +1,6 @@
 import harpicVideo from "/Videos/HARPIC Door to Door.mp4";
 import { Button } from "../components/ui/button";
+import { ChevronRight } from "lucide-react";
 
 const Home = () => {
   return (
@@ -13,9 +14,16 @@ const Home = () => {
       ></video>
       <div className="overlay1">
         <div className="absolute left-10 bottom-48">
-          <Button variant="default">
-            <span className="text-blue-600">GET STARTED</span>
+          <Button variant="default" className="mr-6">
+            <span className="text-white-700">TALK TO US</span>
+
+            <Button variant="link" size="icon">
+              <ChevronRight className="h-4 w-4" />
+            </Button>
           </Button>
+          {/* <Button variant="default">
+            <span className="text-red-700">PARTNERS</span>
+          </Button> */}
         </div>
       </div>
     </div>
