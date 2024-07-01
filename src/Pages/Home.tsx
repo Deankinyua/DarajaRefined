2f29f6d524160e575c2a8a8d091abe47d9c79c38
Dean Kinyua <kinyuadean@gmail.com>
2024-07-01 14:03:02 +0300

    height

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,6 +1,9 @@
 import harpicVideo from "/Videos/HARPIC Door to Door.mp4";
+import video1 from "../../public/Videos/Sunlight Sunny Side campaign.mp4";
 import { Button } from "../components/ui/button";
 import { ChevronRight } from "lucide-react";
+import "../CustomStyles/typography.css";
+import VideoService from "../Services/VideoService";
 
 const Home = () => {
   return (
@@ -22,6 +25,23 @@ const Home = () => {
             </Button>
           </Button>
         </div>
+      </div>
+      <div className="firstSection">
+        <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+          Unlock Your
+          <span className="text-red-600"> Brand's</span> Potential with Our
+          Unique
+          <span className="text-red-600"> Approach</span>
+        </h1>
+        <p className="text-xl">
+          Our dedication to increasing numbers has been the primary reason why
+          we are trusted by the biggest brands. But what really sets us apart?
+          Our commitment to brand campaigns with a purpose. We’re here to make
+          waves and make them count!
+        </p>
+      </div>
+      <div className="videoContainer">
+        <VideoService videoName={video1} />
       </div>
     </div>
   );
