40eb7b8ad7034452aabe325f59efb445304c5686
Dean Kinyua <kinyuadean@gmail.com>
2024-07-01 11:35:13 +0300

    updated and cut the components folder and the utils

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -10,7 +10,7 @@ const buttonVariants = cva(
     variants: {
       variant: {
         default:
-          "bg-blue-800 text-red-600 text-primary-foreground hover:opacity-75",
+          "bg-blue-600 text-red-600 text-primary-foreground hover:opacity-75",
         destructive:
           "bg-destructive text-destructive-foreground hover:bg-destructive/90",
         outline:
