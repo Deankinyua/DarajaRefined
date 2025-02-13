e51b3d54cc64bd3306afa61ed06c943cfb1d751d
Dean Kinyua <kinyuadean@gmail.com>
2025-02-13 13:52:08 +0300

    embedded video

--- src/Pages/About.tsx
+++ src/Pages/About.tsx
@@ -122,8 +122,8 @@ const About = () => {
             title="YouTube video player"
             frameBorder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-            referrerpolicy="strict-origin-when-cross-origin"
-            allowfullscreen
+            referrerPolicy="strict-origin-when-cross-origin"
+            allowFullScreen
           ></iframe>
         </div>
       </section>
