cb5b8ad142ff2f6da7dd99aa3cb611e310f969f4
Dean Kinyua <kinyuadean@gmail.com>
2025-02-13 13:50:14 +0300

    embedded video

--- src/Pages/About.tsx
+++ src/Pages/About.tsx
@@ -120,7 +120,7 @@ const About = () => {
             height="315"
             src="https://www.youtube.com/embed/mzB1VGEGcSU?si=EUBm5kQbhikzfN5J"
             title="YouTube video player"
-            frameborder="0"
+            frameBorder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
             referrerpolicy="strict-origin-when-cross-origin"
             allowfullscreen
